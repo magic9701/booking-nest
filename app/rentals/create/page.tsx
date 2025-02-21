32e4b59758faf490db18d323ef961918d96d7500
Kelly <magic9701@gmail.com>
2025-02-21 19:50:35 +0800

    <Feat> add SelectInput component

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -3,6 +3,8 @@ import FormContainer from '@/components/form/FormContainer';
 import { createPropertyAction } from '@/utils/action'
 import { SubmitButton } from '@/components/form/Buttons';
 import PriceInput from '@/components/form/PriceInput';
+import SelectInput from '@/components/form/SelectInput';
+import { categories } from '@/data/categories'
 
 function CreatePropertyPage() {
   return (
@@ -14,6 +16,16 @@ function CreatePropertyPage() {
             <FormInput name='name' type='text' label='房源名稱' placeholder='請輸入房源名稱' maxLength={20}/>
             <FormInput name='tagline' type='text' label='標語' placeholder='請輸入標語' maxLength={30}/>
             <PriceInput />
+            <SelectInput
+              label="房型類別"
+              name="category"
+              placeholder="請選擇一個類別"
+              items={categories.map(({ key, label, icon }) => ({
+                value: key,
+                label,
+                icon,
+              }))}
+            />
           </div>
           <SubmitButton text='創建房源' />
         </FormContainer>
---
+++ components/form/SelectInput.tsx
@@ -0,0 +1,56 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "../ui/label";
+
+import { IconType } from "react-icons";
+
+type SelectInputProps = {
+  label?: string;
+  name: string;
+  placeholder: string;
+  items: Item[];
+  defaultValue?: string;
+};
+
+type Item = {
+  value: string;
+  label: string;
+  icon: IconType;
+};
+
+function SelectInput(props: SelectInputProps) {
+  return (
+    <div className="mb-2">
+      {props.label && (
+        <Label htmlFor={props.name} className="capitalize">
+          {props.label}
+        </Label>
+      )}
+      <Select name={props.name} defaultValue={props.defaultValue}>
+        <SelectTrigger>
+          <SelectValue placeholder={props.placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {props.items.map((item) => (
+              <SelectItem key={item.value} value={item.value}>
+                <div className="flex items-center gap-2">
+                  <item.icon className="w-5 h-5" />
+                  {item.label}
+                </div>
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
+
+export default SelectInput;
